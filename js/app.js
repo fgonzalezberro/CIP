b177595950273d19258eb388742d5795802771c2
fgonzalezberro <fgonzalezberro@gmail.com>
2020-05-23 04:30:29 -0300

    Update Index Content and Styles

--- js/app.js
+++ js/app.js
@@ -1,5 +1,52 @@
+// Firebase Config
+var firebaseConfig = {
+  apiKey: "AIzaSyB-9B8eFNyEIF2oCpHt4prvMOWLXrBRszk",
+  authDomain: "control-integral-plagas.firebaseapp.com",
+  databaseURL: "https://control-integral-plagas.firebaseio.com",
+  projectId: "control-integral-plagas",
+  storageBucket: "control-integral-plagas.appspot.com",
+  messagingSenderId: "390728418986",
+  appId: "1:390728418986:web:07b172886fdde1c1c072d3",
+  measurementId: "G-EXJFLLE8LL"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 // Charge Document
 window.addEventListener('load', ()=>{
+
+  // Select Loader DOM element
+  const loadingSection = document.querySelector('.loading-section');
+  // Hide loader when Document is charged
+  loadingSection.style.display = 'none';
+
+  // Execute Our Clients Carousel function
+  $('.our-clients-carousel').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    dots: true,
+    infinite: true,
+    arrows: true,
+    slidesToScroll: 2,
+    speed: 400,
+    autoplay: true,
+    slidesToShow: 3
+  });
+
+  // Carousel Arrows
+  const leftArrow = document.querySelector('.fa-arrow-left');
+  const rightArrow = document.querySelector('.fa-arrow-right');
+
+  leftArrow.addEventListener('click' , () =>{
+    document.querySelector('.slick-prev').click();
+  });
+
+  rightArrow.addEventListener('click' , () =>{
+    document.querySelector('.slick-next').click();
+  });
+
   // Capture the principal view element
   const principalView = document.querySelector('.principal-view');
   principalView.classList.add('animated' , 'fadeInDown');
