b5b06dac057657f5d7ae3785f0a47535ffe86565
fgonzalezberro <fgonzalezberro@gmail.com>
2020-07-06 01:32:23 -0300

    Complete login interface and functionality section

--- js/login.js
+++ js/login.js
@@ -1,11 +1,13 @@
+import { ajaxRequest } from './ajax-request.js';
+
 // Login Validation
 const loginValidation = () =>{
   // User Name Validation
   $(document).on('keyup', '.login-username', function(){
-    let loginUserName = document.querySelector('.login-username');
-    let loginUserNameValue = loginUserName.value;
-    let userCheck = document.querySelector('.user-check');
-    let userUnCheck = document.querySelector('.user-un-check');
+    const loginUserName = document.querySelector('.login-username');
+    const loginUserNameValue = loginUserName.value;
+    const userCheck = document.querySelector('.user-check');
+    const userUnCheck = document.querySelector('.user-un-check');
 
     // Username Validation
     if(loginUserNameValue.length > 8 && loginUserNameValue != ''){
@@ -22,10 +24,10 @@ const loginValidation = () =>{
 
   // Password Validation
   $(document).on('keyup', '.login-password', function(){
-    let loginPassword = document.querySelector('.login-password');
-    let loginPasswordValue = loginPassword.value;
-    let passCheck = document.querySelector('.pass-check');
-    let passUnCheck = document.querySelector('.pass-un-check')
+    const loginPassword = document.querySelector('.login-password');
+    const loginPasswordValue = loginPassword.value;
+    const passCheck = document.querySelector('.pass-check');
+    const passUnCheck = document.querySelector('.pass-un-check')
 
     // Password Validation
     if(loginPasswordValue.length > 5 && loginPasswordValue != ''){
@@ -44,27 +46,43 @@ const loginValidation = () =>{
 // Login Users Function
 const loginUsers = () =>{
   $(document).on('click', '.login-button', function(){
-    let loginUserName = document.querySelector('.login-username');
-    let loginPassword = document.querySelector('.login-password');
+    const loginUserName = document.querySelector('.login-username');
+    const loginPassword = document.querySelector('.login-password');
+    const errorMessage = document.querySelector('.error-login-message');
+    const successMessage = document.querySelector('.success-login-message');
+    const userCheck = document.querySelector('.user-check');
+    const userUnCheck = document.querySelector('.user-un-check');
+    const passCheck = document.querySelector('.pass-check');
+    const passUnCheck = document.querySelector('.pass-un-check')
 
     // Check existent user
     firebase.auth().signInWithEmailAndPassword(loginUserName.value , loginPassword.value)
       .catch(function(error) {
-       var errorCode = error.code;
-       var errorMessage = error.message;
-       console.log(errorCode)
-       console.log(errorMessage);
+       // var errorCode = error.code;
+       // var errorMessage = error.message;
+       // console.log(errorCode)
+       // console.log(errorMessage);
 
-       // Show Success Message
-       $('.error-login-message').slideDown();
+       // Show Error Message
+       errorMessage.classList.remove('fadeOutLeft');
+       errorMessage.classList.add('wow' , 'animated' , 'fadeInLeft' , 'slower');
+       errorMessage.style.display = 'flex';
+       userCheck.style.display = 'none';
+       userUnCheck.style.display = 'none';
+       passCheck.style.display = 'none';
+       passUnCheck.style.display = 'none';
 
-       // Hide error message
+       // Hide Error message
        setTimeout(() =>{
-         $('.error-login-message').slideUp();
+         errorMessage.classList.add('fadeOutLeft');
 
          // Clean input fields
          loginUserName.value = '';
          loginPassword.value = '';
+
+         setTimeout(() => {
+           errorMessage.style.display = 'none';
+         },1000);
        }, 3000);
      });
 
@@ -81,20 +99,34 @@ const loginUsers = () =>{
          var providerData = user.providerData;
 
          // Show Success Message
-         $('.success-login-message').slideDown();
+         successMessage.classList.remove('fadeOutLeft');
+         successMessage.classList.add('wow' , 'animated' , 'fadeInLeft' , 'slow');
+         successMessage.style.display = 'flex';
+         userCheck.style.display = 'none';
+         userUnCheck.style.display = 'none';
+         passCheck.style.display = 'none';
+         passUnCheck.style.display = 'none';
 
+         // Hide Success Message
          setTimeout(() =>{
-           // Hide Success Message
-           $('.success-login-message').slideUp();
-         }, 1500);
+           successMessage.classList.add('fadeOutLeft');
+
+           // Clean input fields
+           loginUserName.value = '';
+           loginPassword.value = '';
+
+           setTimeout(() =>{
+             successMessage.style.display = 'none';
+           }, 1000);
+         }, 3000);
 
          setTimeout(() =>{
            if(uid === 'YEvESS87g5aEEKpY9xxZvmZE8xt2'){
-              alert('Admin User');
+              ajaxRequest('../components/admin-dashboard.html');
             }else{
               alert('Normal User');
             }
-         }, 2500);
+         }, 4300);
        }
        else {
          // User is signed out.
