a76cc0881687dd439070172fe02f1ef8d1f0172f
fgonzalezberro <fgonzalezberro@gmail.com>
2020-07-07 23:55:18 -0300

    Update change password validation features

--- js/change-password.js
+++ js/change-password.js
@@ -52,17 +52,61 @@ const changePassword = () =>{
   const confirmPass = document.querySelector('.confirm-pass');
   const newPassValue = newPass.value;
   const confirmPassValue = confirmPass.value;
+  const successMessage = document.querySelector('.success-change-password-message');
+  const errorMessage = document.querySelector('.error-change-password-message');
+  const newPassCheck = document.querySelector('.new-pass-check');
+  const newPassUnCheck = document.querySelector('.new-pass-un-check');
+  const confirmPassCheck = document.querySelector('.confirm-new-pass-check');
+  const confirmPassUnCheck = document.querySelector('.confirm-new-pass-un-check');
 
   const user = firebase.auth().currentUser;
 
   if(newPassValue === confirmPassValue && newPassValue.length >= 6 && confirmPassValue.length >= 6){
     user.updatePassword(newPassValue).then(function() {
-      alert('Contraseña cambiada con exito');
+      successMessage.classList.remove('fadeOutLeft');
+      successMessage.classList.add('wow' , 'animated' , 'fadeInLeft' , 'slow');
+      successMessage.style.display = 'flex';
+      confirmPassUnCheck.style.display = 'none';
+      confirmPassCheck.style.display = 'none';
+      newPassUnCheck.style.display = 'none';
+      newPassCheck.style.display = 'none';
+
+      // Hide Success Message
+      setTimeout(() =>{
+        successMessage.classList.add('fadeOutLeft');
+
+        // Clean input fields
+        newPass.value = '';
+        confirmPass.value = '';
+
+        setTimeout(() =>{
+          successMessage.style.display = 'none';
+        }, 1000);
+      }, 3000);
     }).catch(function(error) {
       alert('No se puedo cambiar la contrasña');
     });
   }else{
-    alert('Por favor chequea que el usuario y contraseña coincidan y tengan más de 6 caracteres')
+    errorMessage.classList.remove('fadeOutLeft');
+    errorMessage.classList.add('wow' , 'animated' , 'fadeInLeft' , 'slower');
+    errorMessage.style.display = 'flex';
+    confirmPassUnCheck.style.display = 'none';
+    confirmPassCheck.style.display = 'none';
+    newPassUnCheck.style.display = 'none';
+    newPassCheck.style.display = 'none';
+
+    // Hide Error message
+    setTimeout(() =>{
+      errorMessage.classList.add('fadeOutLeft');
+
+      // Clean input fields
+      newPass.value = '';
+      confirmPass.value = '';
+
+      setTimeout(() => {
+        errorMessage.style.display = 'none';
+      },1000);
+    }, 3000);
   }
 }
 
