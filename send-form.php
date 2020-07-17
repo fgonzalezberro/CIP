34160817538bd75906d28cdd29a7f51e62b815dc
fgonzalezberro <fgonzalezberro@gmail.com>
2020-07-17 06:09:54 -0300

    Debug php email sender

--- send-form.php
+++ send-form.php
@@ -4,6 +4,9 @@
    $affair = $_POST["affair"];
    $message = $_POST["message"];
 
+   // Email data
+   $emailData = 'admin@cip.com.uy';
+
    $emailMessage = "De: $email \n";
    $emailMessage .= "E-mail: $email \n";
    $emailMessage .= "Asunto: $affair \n";
@@ -13,7 +16,7 @@
    $aditionalHeaders = "From: $email \r\n";
 
    //Send Email
-   $mail = @mail($affair, $emailMessage, $aditionalHeaders);
+   $mail = @mail($emailData, $affair, $emailMessage, $aditionalHeaders);
 
    if($mail){
       echo '<div style= "height: 100vh; width:100%; display: flex; flex-direction: column; align-items:center; justify-content: center;"><p style="border: 2px solid #0c82c6; color: #0c82c6; padding: 5% 10%; margin-bottom: 2%;"> El email se envío correctamente</p> <a href="index.html"> <button style= "padding: 10px 20px; border: 2px solid #0c82c6; background-color: #0c82c6; color: #fff; cursor: pointer;">Volver al sitio</button></a></div>';
